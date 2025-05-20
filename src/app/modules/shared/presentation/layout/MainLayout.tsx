d03adaedd1723256158bcc7303486535b72b13f5
jagoqui <jaidiver.gomez@udea.edu.co>
2025-05-20 02:42:05 -0500

    Feat updated

--- src/app/modules/shared/presentation/layout/MainLayout.tsx
+++ src/app/modules/shared/presentation/layout/MainLayout.tsx
@@ -1,19 +1,10 @@
-import { Link, Outlet } from "react-router";
-import { Toaster } from "sonner";
-import { PATH_NAMES } from "../../domain/schemas/pathnames.schema";
+import { Outlet } from "react-router"
+import { Toaster } from "sonner"
 
 export const MainLayout = () => {
-  return (
+    return (
     <>
-      <Link aria-label="Go to Home" to={PATH_NAMES.HOME}>
-        <h1 className="text-3xl font-bold underline">Home</h1>
-      </Link>
-      <Link aria-label="List of Users" to={PATH_NAMES.USERS}>
-        <h1 className="text-3xl font-bold underline">Users</h1>
-      </Link>
-
-      <Outlet />
-      <Toaster expand={true} richColors />
-    </>
-  );
-};
+        <Outlet />
+        <Toaster expand={true} richColors />
+    </>)
+}

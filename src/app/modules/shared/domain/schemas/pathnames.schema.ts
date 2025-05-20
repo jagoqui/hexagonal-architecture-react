3948d78de5d4d54dbd09a89d441ba6ff5fc48485
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-05-20 02:26:02 -0500

    Refactors routing and layout with improved structure
    Updates pathnames schema for consistency, enhances router configuration with a wildcard redirect, and adds navigation links to MainLayout for better user experience. Refactors code style for improved readability and maintainability.

--- src/app/modules/shared/domain/schemas/pathnames.schema.ts
+++ src/app/modules/shared/domain/schemas/pathnames.schema.ts
@@ -1,12 +1,11 @@
-import { z } from 'zod';
-const BASE_PATH = '/';
+import { z } from "zod";
+const BASE_PATH = "/";
 
 export const PATH_NAMES = {
-    HOME: BASE_PATH,
-    USERS: `${BASE_PATH}/users` 
+  HOME: BASE_PATH,
+  USERS: `${BASE_PATH}users`,
 } as const satisfies Record<string, string>;
-
 
 export const patnamesSchema = z.custom<typeof PATH_NAMES>();
 
-export type Pathnames = z.infer<typeof patnamesSchema>
+export type Pathnames = z.infer<typeof patnamesSchema>;
--- src/app/modules/shared/presentation/layout/MainLayout.tsx
+++ src/app/modules/shared/presentation/layout/MainLayout.tsx
@@ -1,10 +1,19 @@
-import { Outlet } from "react-router"
-import { Toaster } from "sonner"
+import { Link, Outlet } from "react-router";
+import { Toaster } from "sonner";
+import { PATH_NAMES } from "../../domain/schemas/pathnames.schema";
 
 export const MainLayout = () => {
-    return (
+  return (
     <>
-        <Outlet />
-        <Toaster expand={true} richColors />
-    </>)
-}
+      <Link aria-label="Go to Home" to={PATH_NAMES.HOME}>
+        <h1 className="text-3xl font-bold underline">Home</h1>
+      </Link>
+      <Link aria-label="List of Users" to={PATH_NAMES.USERS}>
+        <h1 className="text-3xl font-bold underline">Users</h1>
+      </Link>
+
+      <Outlet />
+      <Toaster expand={true} richColors />
+    </>
+  );
+};
